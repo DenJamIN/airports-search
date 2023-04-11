1f88b7284111e8bf782d6211040efb44798c0cf9
Denjamin <denlavre@mail.ru>
2023-04-11 15:28:34 +0300

    написан тест для SearchAirports

---
+++ src/test/java/org/example/SearchAirportsTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+public class SearchAirportsTest {
+
+    List<List<String>> airports;
+
+    @Before
+    public void init() throws IOException {
+        ReadCSV readCSV = new ReadCSV();
+        airports = readCSV.read();
+    }
+
+    @Test
+    public void testSearch() {
+        SearchAirports searchAirports = new SearchAirports(airports, "Bo");
+        long start = System.currentTimeMillis();
+        List<List<String>> actual = searchAirports.search();
+        long finish = System.currentTimeMillis();
+        long elapsed = finish - start;
+        Assert.assertFalse("Время выполнения программы: " + elapsed + " (дольше 25мс)", elapsed > 25L);
+    }
+}
